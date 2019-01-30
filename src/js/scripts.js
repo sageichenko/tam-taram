7383c057d8538973150ce0272b685eef0c037933
user <user@iMac-user.oggy.co>
2019-01-30 13:34:46 +0300

    Add changes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,103 +1,84 @@
 //import debounce from './modules/debounce';
 
-function debounce(func, ms) {
-    let timer = null;
+// function debounce(func, ms) {
+//     let timer = null;
+//
+//     return function () {
+//         if (timer) {
+//             clearTimeout(timer);
+//         }
+//         timer = setTimeout(() => {
+//             timer = null;
+//             func(...[].slice.call(arguments));
+//         }, ms);
+//     };
+// }
 
-    return function () {
-        if (timer) {
-            clearTimeout(timer);
-        }
-        timer = setTimeout(() => {
-            timer = null;
-            func(...[].slice.call(arguments));
-        }, ms)
-    }
-}
-
-class Slider {
-    constructor($slider) {
-        this.el = $slider;
-        this._pos = 0;
-        this._initValues();
-        this.toggleAvailableMod(this.el);
-        this._initEvents();
-
-    }
-
-    _initEvents() {
-        this.el.addEventListener('click', event => {
-            const $target = event.target;
-            if ($target.classList.contains('arrow') && $target.classList.contains('_available')) {
-                this.slide($target.getAttribute('data-direction'), this._list);
-                this.toggleAvailableMod(this.el);
-            }
-        });
-
-        window.addEventListener('resize', debounce(this._initValues.bind(this), 1000));
-    }
-
-    _initValues() {
-        this._list = this.el.querySelector('.slider__list');
-        this._sliderWidth = this.el.offsetWidth;
-        console.log(this._sliderWidth);
-        this._itemQty = this._list.childElementCount;
-        this._itemWidth = this.el.querySelector('.slider__list-item').offsetWidth;
-        this._offset = this.el.querySelectorAll('.slider__list-item')[1].offsetLeft - this.el.querySelectorAll('.slider__list-item')[0].offsetLeft - this._itemWidth; //(this._sliderWidth - this._visibleItemQty * this._itemWidth) / (this._visibleItemQty - 1);
-        this._visibleItemQty = Math.ceil((this._sliderWidth + this._offset) / (this._itemWidth + this._offset));
-        console.log(this._visibleItemQty);
-        this._scrollWidth = this._sliderWidth + this._offset;
-        this._minPos = -Math.floor(this._itemQty * this._itemWidth + (this._itemQty - 1) * this._offset - this._sliderWidth);  //-(Math.floor((this._itemQty - this._visibleItemQty) / this._visibleItemQty)) * this._scrollWidth - this._itemQty % this._visibleItemQty * (this._itemWidth + this._offset);
-
-    }
-
-    changePosRight() {
-        return Math.max(this._pos - this._scrollWidth, this._minPos);
-    }
-
-    changePosLeft() {
-        return Math.min(this._pos + this._scrollWidth, 0);
-    }
-
-    slide(direction, $el) {
-        if (direction === 'right') {
-            this._pos = this.changePosRight();
-
-        } else if (direction === 'left') {
-            this._pos = this.changePosLeft();
-        }
-        console.log(this._pos);
-        $el.style.left = `${this._pos}px`;
-    }
-
-    removeAvailableMod($el) {
-        $el.classList.remove('_available');
-    }
-
-    addAvailableMod($el) {
-        $el.classList.add('_available');
-    }
-
-    toggleAvailableMod() {
-        if (this._pos === 0) {
-            this.removeAvailableMod(this.el.querySelector('.arrow._left'));
-        } else {
-            this.addAvailableMod(this.el.querySelector('.arrow._left'));
-        }
-        if (this._pos === this._minPos) {
-            this.removeAvailableMod(this.el.querySelector('.arrow._right'));
-        } else {
-            this.addAvailableMod(this.el.querySelector('.arrow._right'));
-        }
-
-    }
-}
-/*-----------------------------------------------------------------------------------------------------------------------*/
+//const turningWidth = 425;
 
 document.addEventListener('DOMContentLoaded', function () {
-    const sliders = [].map.call(document.getElementById('gallery').querySelectorAll('.slider'), item => {
-        return new Slider(item);
-    });
+    initSlider('.slider');
 });
+
+function initSlider(selector) {
+    $(selector).slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        mobileFirst: true,
+        rows: 3,
+        arrows: false,
+        vertical: true,
+
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 590,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    rows: 1,
+                    arrows: true,
+                    vertical: false,
+                }
+            }
+        ]
+    });
+    //window.addEventListener('resize', handlerScreenDecrease);
+}
+
+// function destroySlider(selector) {
+//     $(selector).slick('unslick');
+//     console.log('unslick');
+// }
+//
+// const handlerScreenDecrease = debounce( (ev) => {
+//     const window = ev.target;
+//     const width = window.innerWidth;
+//     if (width <= turningWidth) {
+//         destroySlider('.slider');
+//     }
+//
+//     window.removeEventListener(handlerScreenDecrease);
+//     window.addEventListener('resize', handlerScreenIncrease);
+// }, 1000);
+//
+// const handlerScreenIncrease = debounce ( (ev) => {
+//     const window = ev.target;
+//     const width = window.innerWidth;
+//     if (width >= turningWidth) {
+//         initSlider('.slider');
+//     }
+//
+//     window.removeEventListener(handlerScreenIncrease);
+//     window.addEventListener('resize', handlerScreenDecrease);
+// }, 1000);
 
 
 
